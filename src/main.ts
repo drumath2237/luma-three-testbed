cb7a2acc8fc0e525c4c3d4ff218a2bca61b7bf2e
drumath2237 <2237k.drumath@gmail.com>
2023-12-17 23:10:53 +0900

    :tada: created threejs scene

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
+import {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "three";
+// @ts-ignore
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
 import "./style.scss";
 
 const main = async () => {
@@ -8,7 +19,33 @@ const main = async () => {
     return;
   }
 
-  console.log("hi");
+  const scene = new Scene();
+  const camera = new PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 5;
+
+  const renderer = new WebGLRenderer({ antialias: true, canvas: renderCanvas });
+  renderer.setClearColor(0xffffff);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  const cube = new Mesh(
+    new BoxGeometry(1, 1, 1),
+    new MeshBasicMaterial({ color: 0x00ff00 })
+  );
+  scene.add(cube);
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    controls.update();
+  };
+  animate();
 };
 
 main();
